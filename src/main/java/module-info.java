dc6be64e5461910b9d956a950fe4fe232ce4c5df
Ralph Niemitz <ralph.niemitz@gmx.de>
2018-07-24 13:31:55 +0200

    24.07.2018
    Signed-off-by: Ralph Niemitz <ralph.niemitz@gmx.de>

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.ralleytn.plugins.jinput.xinput {
-	
-	requires transitive jinput;
-	requires transitive jutils;
-	requires de.ralleytn.wrapper.microsoft.xinput;
-	
-	exports de.ralleytn.plugins.jinput.xinput;
-}
